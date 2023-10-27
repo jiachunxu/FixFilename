54ca44b336bee2018bde37bc8dec0d31ea045c64
jiacx <954951185@qq.com>
2023-10-27 11:43:58 +0800

    +1

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,13 @@ public class Main {
 
     public static void main(String[] args) {
 //        File file = new File("D:\\aria2\\专业录制");
-        File file = new File("D:\\aria2\\test");
-        Tools.RemoveFile(file);
+        File path = new File("D:\\aria2\\test");
+        Tools.RemoveFile(path);
+        Tools.FixFileName(path, "-账号已注销-账号已注销-哔哩哔哩视频");
+        Tools.FixFileName(path, "斗鱼-");
+        Tools.FixFileName(path, "网易CC-");
+        Tools.FixFileName(path, "抖音-");
+        System.out.println("完成");
 
     }
 
--- src/Tools.java
+++ src/Tools.java
@@ -11,15 +11,16 @@ public class Tools {
 
     /**
      * @param pathName 目录
-     *                 删除数字后缀的文件(不删除子目录)
+     *                 <p>
+     *                 删除目录下数字后缀的文件(不删除子目录)
      */
     public static void RemoveFile(File pathName) {
         if (!pathName.isDirectory()) {
             return;
         }
-        List<File> collect = Arrays
-                .stream(Objects.requireNonNull(pathName.listFiles()))
-                .filter(File::isFile).toList();
+        List<File> collect = Arrays.stream(Objects.requireNonNull(pathName.listFiles()))
+                .filter(File::isFile)
+                .toList();
         if (collect.isEmpty()) {
             return;
         }
@@ -33,6 +34,34 @@ public class Tools {
             }
             file.delete();
         }
+    }
+
+    /**
+     * @param pathName   目录
+     * @param removePart 删除部分文件名
+     */
+    public static void FixFileName(File pathName, String removePart) {
+        if (!pathName.isDirectory() || removePart == null || removePart.isEmpty()) {
+            return;
+        }
+        List<File> collect = Arrays.stream(Objects.requireNonNull(pathName.listFiles()))
+                .filter(File::isFile)
+                .toList();
+        if (collect.isEmpty()) {
+            return;
+        }
+        for (File file : collect) {
+            String name = file.getName();
+            if (!name.contains(removePart)) {
+                continue;
+            }
+            String reName = name.replace(removePart, "");
+            File file1 = new File(file.getParent(), reName);
+            file.renameTo(file1);
+
+        }
+
+
     }
 
 }
